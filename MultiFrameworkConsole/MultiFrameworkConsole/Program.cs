0f968f79f72fc3bab7fda4642dc94cd482d2d08f
Akila Ranasinghe <ucsc.akila@gmail.com>
2018-04-10 17:10:17 +0530

    ASP Core apps,Web API

---
+++ MultiFrameworkConsole/MultiFrameworkConsole/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MultiFrameworkConsole
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+#if NET40
+            Console.WriteLine("Target framework: .net 4.0");
+#elif NET45
+            Console.WriteLine("Target framework: .net 4.5");
+#else
+            Console.WriteLine("Target framework: .net core");
+#endif
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MyWebAPI/API/Global.asax.cs
@@ -0,0 +1,53 @@
+﻿using API.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Routing;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace API
+{
+    public class Global : System.Web.HttpApplication
+    {
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            //AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(APIConfig.Register);
+            //RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
