373eb7d43d22daed5d6b2c4fead304377c7f8fbb
Akila Ranasinghe <ucsc.akila@gmail.com>
2018-04-17 08:36:36 +0530

    DP

---
+++ HRCore/HRCore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HRCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
